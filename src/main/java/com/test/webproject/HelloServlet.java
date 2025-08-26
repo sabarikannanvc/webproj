6bb6495f478e051339810ecf8ed9e819e7c1e07a
Jothi Kannan <jothi_kannan@hcl.com>
2025-08-26 12:33:05 +0530

    first commit

---
+++ src/main/java/com/test/webproject/HelloServlet.java
@@ -0,0 +1,22 @@
+package com.test.webproject;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().println("Hello Servlet from webproject");
+	}
+
+}
